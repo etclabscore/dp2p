9a64e832801a12ee2c9a9477fd1d2c5c751c0c55
ia <isaac.ardis@gmail.com>
2019-02-22 09:46:34 -0600

    add timeout flag, comment unused flags

--- cmd/root.go
+++ cmd/root.go
@@ -26,12 +26,12 @@ import (
 	elog "github.com/ethereum/go-ethereum/log"
 	"github.com/ethereum/go-ethereum/p2p"
 	"github.com/ethereum/go-ethereum/p2p/enode"
-	homedir "github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 var cfgFile string
+var connectTimeout int
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
@@ -96,7 +96,7 @@ var rootCmd = &cobra.Command{
 		go func() {
 			// We can't listen for dial failures w/o too much hacking.
 			// So we just set a timeout to wait for a positive connection.
-			t := time.NewTicker(30 * time.Second)
+			t := time.NewTicker(time.Duration(int32(connectTimeout)) * time.Second)
 			defer serv.Stop()
 			for {
 				select {
@@ -143,39 +143,41 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().IntVarP(&connectTimeout, "timeout", "t", 30, "time in seconds to wait for node to dial a connection")
 
 	// Here you will define your flags and configuration settings.
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
-	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.devp2ping")
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.devp2ping")
 
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
 // initConfig reads in config file and ENV variables if set.
 func initConfig() {
-	if cfgFile != "" {
-		// Use config file from the flag.
-		viper.SetConfigFile(cfgFile)
-	} else {
-		// Find home directory.
-		home, err := homedir.Dir()
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
+	// if cfgFile != "" {
+	// 	// Use config file from the flag.
+	// 	viper.SetConfigFile(cfgFile)
+	// } else {
+	// 	// Find home directory.
+	// 	home, err := homedir.Dir()
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		os.Exit(1)
+	// 	}
 
-		// Search config in home directory with name ".devp2ping" (without extension).
-		viper.AddConfigPath(home)
-		viper.SetConfigName(".devp2ping")
-	}
+	// 	// Search config in home directory with name ".devp2ping" (without extension).
+	// 	viper.AddConfigPath(home)
+	// 	viper.SetConfigName(".devp2ping")
+	// }
 
 	viper.AutomaticEnv() // read in environment variables that match
 
-	// If a config file is found, read it in.
-	if err := viper.ReadInConfig(); err == nil {
-		fmt.Println("Using config file:", viper.ConfigFileUsed())
-	}
+	// // If a config file is found, read it in.
+	// if err := viper.ReadInConfig(); err == nil {
+	// 	fmt.Println("Using config file:", viper.ConfigFileUsed())
+	// }
+
 }
