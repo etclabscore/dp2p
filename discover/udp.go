1ff96f8efb03d9aae172ca3626c2bb5e4de37634
ia <isaac.ardis@gmail.com>
2019-03-25 08:08:47 -0400

    comment udp neighbors log line; they are returned anyway

--- discover/udp.go
+++ discover/udp.go
@@ -355,7 +355,7 @@ func (t *Udp) findnode(toid enode.ID, toaddr *net.UDPAddr, target encPubkey) ([]
 				log.Trace("Invalid neighbor node received", "ip", rn.IP, "addr", toaddr, "err", err)
 				continue
 			}
-			log.Info("Neighbor", "n", n.String())
+			//log.Info("Neighbor", "n", n.String())
 			nodes = append(nodes, n)
 		}
 		return true, nreceived >= bucketSize
